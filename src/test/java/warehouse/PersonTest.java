6cd0af3750d1fdb9595d887bdce80a289893be1f
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-04-13 16:21:46 +0200

    Add Person tests, including file I/O

--- src/test/java/warehouse/PersonTest.java
+++ src/test/java/warehouse/PersonTest.java
@@ -1,118 +1,238 @@
 package warehouse;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class PersonTest {
 
     private Person personUnderTest;
 
-    private String testId = "123";
-    private String testFirstName = "John";
-    private String testLastName = "Doe";
-    private String testEmail = "jdoe@test.com";
+    private static final String[] DEF = {"00000001A", "Name", "LastName", "email@example.com"};
+
+    private final String testId = "123";
+    private final String testFirstName = "John";
+    private final String testLastName = "Doe";
+    private final String testEmail = "jdoe@test.com";
 
     private ArrayList<String> testParams;
 
     @Before
     public void setUp() {
         personUnderTest = new Person(testId, testFirstName, testLastName, testEmail);
-        testParams = new ArrayList<>();
+        testParams = new ArrayList<String>();
         testParams.add(testId);
         testParams.add(testFirstName);
         testParams.add(testLastName);
         testParams.add(testEmail);
     }
 
+
     @Test
     public void testDefaultConstructor() {
         Person defaultPerson = new Person();
-        Assert.assertEquals("00000001A", defaultPerson.getId());
-        Assert.assertEquals("Name", defaultPerson.getFirstName());
-        Assert.assertEquals("LastName", defaultPerson.getLastName());
-        Assert.assertEquals("email@example.com", defaultPerson.getEmail());
+        assertEquals(DEF[0], defaultPerson.getId());
+        assertEquals(DEF[1], defaultPerson.getFirstName());
+        assertEquals(DEF[2], defaultPerson.getLastName());
+        assertEquals(DEF[3], defaultPerson.getEmail());
+    }
+
+    @Test
+    public void testFullConstructor() {
+        Person fullPerson = new Person(testId, testFirstName, testLastName, testEmail);
+        assertEquals(testId, fullPerson.getId());
+        assertEquals(testFirstName, fullPerson.getFirstName());
+        assertEquals(testLastName, fullPerson.getLastName());
+        assertEquals(testEmail, fullPerson.getEmail());
+        assertTrue(fullPerson.equals(personUnderTest));
     }
 
     @Test
     public void testStringConstructor() {
-        Person stringPerson = new Person("|" + String.join("|", testParams));
-        Assert.assertEquals(testId, stringPerson.getId());
-        Assert.assertEquals(testFirstName, stringPerson.getFirstName());
-        Assert.assertEquals(testLastName, stringPerson.getLastName());
-        Assert.assertEquals(testEmail, stringPerson.getEmail());
+        Person stringPerson = new Person(String.join("|", testParams));
+        assertEquals(testId, stringPerson.getId());
+        assertEquals(testFirstName, stringPerson.getFirstName());
+        assertEquals(testLastName, stringPerson.getLastName());
+        assertEquals(testEmail, stringPerson.getEmail());
     }
 
     @Test
     public void testArrayListConstructor() {
         Person arrayListPerson = new Person(testParams);
-        Assert.assertEquals(testId, arrayListPerson.getId());
-        Assert.assertEquals(testFirstName, arrayListPerson.getFirstName());
-        Assert.assertEquals(testLastName, arrayListPerson.getLastName());
-        Assert.assertEquals(testEmail, arrayListPerson.getEmail());
-
-        ArrayList<String> invalidParams = new ArrayList<>();
-        invalidParams.add(testId);
-        invalidParams.add(testFirstName);
-        invalidParams.add(testLastName);
-        invalidParams.add(testEmail);
-        invalidParams.add("invalidParam");
-        try {
-            new Person(invalidParams);
-            Assert.fail();
-        } catch (IllegalArgumentException e) {
-            // Invalid number of data fields
-        }
+        assertEquals(testId, arrayListPerson.getId());
+        assertEquals(testFirstName, arrayListPerson.getFirstName());
+        assertEquals(testLastName, arrayListPerson.getLastName());
+        assertEquals(testEmail, arrayListPerson.getEmail());
     }
 
     @Test
-    public void testSetAndGet() {
+    public void testInvalidArrayListConstructor() {
+        ArrayList<String> invalidParams = new ArrayList<String>() {{
+            addAll(testParams);
+            add("invalidParam");
+        }};
+        assertThrows(IllegalArgumentException.class, () -> new Person(invalidParams));
+    }
+
+    @Test
+    public void testInvalidStringConstructor() {
+        String invalidString = String.join("|", testParams) + "|invalidParam";
+        assertThrows(IllegalArgumentException.class, () -> new Person(invalidString));
+    }
+
+    @Test
+    public void testNonsenseStringConstructor() {
+        String nonsenseString = "nonsense";
+        assertThrows(IllegalArgumentException.class, () -> new Person(nonsenseString));
+    }
+
+
+    @Test
+    public void testSetWithStringArray() {
         String[] data = {
                 "456",
                 "Jane",
                 "Doe",
                 "jane@test.com"
         };
-
         personUnderTest.set(data);
 
-        Assert.assertEquals(data[0], personUnderTest.getId());
-        Assert.assertEquals(data[1], personUnderTest.getFirstName());
-        Assert.assertEquals(data[2], personUnderTest.getLastName());
-        Assert.assertEquals(data[3], personUnderTest.getEmail());
-
-        try {
-            String invalidVarId = "invalidVar";
-            personUnderTest.get(invalidVarId);
-            Assert.fail("Expected exception not thrown.");
-        } catch (IllegalArgumentException e) {
-            Assert.assertTrue(e.getMessage().contains("Invalid varId"));
-        } catch (Exception e) {
-            Assert.fail("Unexpected exception thrown.");
+        assertEquals(data[0], personUnderTest.getId());
+        assertEquals(data[1], personUnderTest.getFirstName());
+        assertEquals(data[2], personUnderTest.getLastName());
+        assertEquals(data[3], personUnderTest.getEmail());
+    }
+    @Test
+    public void testSetWithStringArgs() {
+        String id = "456";
+        String firstName = "Jane";
+        String lastName = "Doe";
+        String email = "jane@test.com";
+        personUnderTest.set(id, firstName, lastName, email);
+        assertEquals(id, personUnderTest.getId());
+        assertEquals(firstName, personUnderTest.getFirstName());
+        assertEquals(lastName, personUnderTest.getLastName());
+        assertEquals(email, personUnderTest.getEmail());
+    }
+    @Test
+    public void testSetWithString() {
+        String[] data = {
+                "456",
+                "Jane",
+                "Doe",
+                "jane@test.com"
+        };
+        String dataString = String.join("|", data);
+        personUnderTest.set(dataString);
+        assertEquals(data[0], personUnderTest.getId());
+        assertEquals(data[1], personUnderTest.getFirstName());
+        assertEquals(data[2], personUnderTest.getLastName());
+        assertEquals(data[3], personUnderTest.getEmail());
+    }
+    @Test
+    public void testSetWithVarIdAndValue() {
+        String[] data = {
+                "456",
+                "Jane",
+                "Doe",
+                "jane@test.com"
+        };
+        String[] varIds = {
+                "id",
+                "firstName",
+                "lastName",
+                "email"
+        };
+        for (int i = 0; i < data.length; i++) {
+            personUnderTest.set(varIds[i], data[i]);
+            assertEquals(data[i], personUnderTest.get(varIds[i]));
         }
+        assertEquals(data[0], personUnderTest.getId());
+        assertEquals(data[1], personUnderTest.getFirstName());
+        assertEquals(data[2], personUnderTest.getLastName());
+        assertEquals(data[3], personUnderTest.getEmail());
+    }
 
-        HashMap<String, String> validVarIdsAndValues = new HashMap<>();
-        validVarIdsAndValues.put("id", data[0]);
-        validVarIdsAndValues.put("firstName", data[1]);
-        validVarIdsAndValues.put("lastName", data[2]);
-        validVarIdsAndValues.put("email", data[3]);
+    @Test
+    public void testSetWithInvalidString() {
+        String invalidString = "invalidString";
+        assertThrows(IllegalArgumentException.class, () -> personUnderTest.set(invalidString));
+    }
+    @Test
+    public void testSetWithInvalidStringArray() {
+        String[] invalidStringArray = {"1248", "Jane", "Doe"}; //No email
+        assertThrows(IllegalArgumentException.class, () -> personUnderTest.set(invalidStringArray));
+    }
 
-        for (Map.Entry<String, String> entry : validVarIdsAndValues.entrySet()) {
-            String varId = entry.getKey();
-            String expectedValue = entry.getValue();
-            String actualValue = personUnderTest.get(varId);
-            Assert.assertEquals(expectedValue, actualValue);
+
+    @Test
+    public void testGetWithAllVarIds() {
+        String[] varIds = {
+                "id",
+                "firstName",
+                "lastName",
+                "email"
+        };
+        for (int i = 0; i < varIds.length; i++) {
+            assertEquals(testParams.get(i), personUnderTest.get(varIds[i]));
         }
+    }
+    @Test
+    public void testGetWithInvalidVarId() {
+        String invalidVarId = "invalidVarId";
+        assertThrows(IllegalArgumentException.class, () -> personUnderTest.get(invalidVarId));
     }
 
     @Test
     public void testToString() {
         String expectedString = "(" + String.join("|", testParams) + ")";
         String actualString = personUnderTest.toString();
-        Assert.assertEquals(expectedString, actualString);
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void testWriteToFile() {
+        String filepath = "src\\test\\tmp\\testWriteToFile.txt";
+
+        String expectedString = "(" + String.join("|", testParams) + ")";
+
+        try {
+            File file = new File(filepath);
+            file.createNewFile();
+            Scanner scanner = new Scanner(file);
+
+            personUnderTest.writeToFile(filepath);
+            String fileContent = scanner.nextLine();
+            assertEquals(fileContent, expectedString);
+            scanner.close();
+        }
+        catch (Exception e) {
+            fail(e.getStackTrace().toString());
+        }
+    }
+
+    @Test
+    public void testFromFile() {
+        String filepath = "src\\test\\tmp\\testFromFile.txt";
+        String personString = "(" + String.join("|", testParams) + ")";
+
+        try {
+            File file = new File(filepath);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+            writer.write(personString);
+            writer.close();
+
+            Person personFromFile = Person.fromFile(filepath);
+            assertTrue(personFromFile.equals(personUnderTest));
+        }
+        catch (Exception e) {
+            fail(e.getStackTrace().toString());
+        }
     }
 }
