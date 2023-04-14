f4dd454d9b88cfd74e300fa9eda582302f8d2f6f
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-04-14 14:23:10 +0200

    Add first endpoint (/helloworld)

--- src/main/java/app/RunApp.java
+++ src/main/java/app/RunApp.java
@@ -8,14 +8,22 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class RunApp {
-
 	
-	public static void main(String[] args) throws SAXException, IOException {
-		SpringApplication.run(RunApp.class, args);
-	}
+    public static void main(String[] args) throws SAXException, IOException {
+      SpringApplication.run(RunApp.class, args);
+    }
+
+    @GetMapping("/helloworld")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
