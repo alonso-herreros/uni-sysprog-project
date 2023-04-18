b6db0be01d8ccbd579c42798faa6a053b9103e83
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-04-18 18:50:02 +0200

    Update ProductList to imlement List instead of Collection

--- src/main/java/store/ProductList.java
+++ src/main/java/store/ProductList.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class ProductList extends WarehouseElement implements Collection<StockableProduct> {
+public class ProductList extends WarehouseElement implements List<StockableProduct> {
 
     protected ArrayList<StockableProduct> list = new ArrayList<StockableProduct>();
     protected double totalCost, totalPrice, totalBenefit;
@@ -91,7 +93,17 @@ public class ProductList extends WarehouseElement implements Collection<Stockabl
     public double getTotalBenefit() { return totalBenefit; }
 
 
-    // Collection methods
+    // List methods
+    @Override
+    public StockableProduct get(int index) {
+        return list.get(index);
+    }
+    @Override
+    public StockableProduct set(int index, StockableProduct element) {
+        StockableProduct out = list.remove(index);
+        list.add(index, element);
+        return out;
+    }
     @Override
     public int size() {
         return list.size();
@@ -101,56 +113,50 @@ public class ProductList extends WarehouseElement implements Collection<Stockabl
         return list.isEmpty();
     }
     @Override
-    public boolean contains(Object o) {
-        return list.contains(o);
-    }
-    @Override
-    public Iterator<StockableProduct> iterator() {
-        return list.iterator();
-    }
-    @Override
-    public Object[] toArray() {
-        return list.toArray();
-    }
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return list.toArray(a);
-    }
-    @Override
     public boolean add(StockableProduct e) {
-        if (list.add(e)) {
-            getters.put(Integer.toString(list.size()-1), () -> e.toString());
-            setters.put(Integer.toString(list.size()-1), (String data) -> e.set(data));
-            totalCost += e.getTotalCost();
-            totalPrice += e.getTotalPrice();
-            totalBenefit = totalCost-totalPrice;
-            return true;
-        }
-        return false;
-    }
+        add(size(), e);
+        return true;
+    } // vv FALL THROUGH vv
     @Override
-    public boolean remove(Object o) {
-        final int index = list.indexOf(o);
-        if (list.remove(o)) {
-            getters.remove(Integer.toString(index));
-            setters.remove(Integer.toString(index));
-            totalCost -= ((StockableProduct) o).getTotalCost();
-            totalPrice -= ((StockableProduct) o).getTotalPrice();
-            totalBenefit = totalCost-totalPrice;
-            return true;
-        }
-        return false;
-    }
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return list.containsAll(c);
+    public void add(int index, StockableProduct e) {
+        list.add(index, e);
+        getters.put(Integer.toString(list.size()-1), () -> e.toString());
+        setters.put(Integer.toString(list.size()-1), (String data) -> e.set(data));
+        totalCost += e.getTotalCost();
+        totalPrice += e.getTotalPrice();
+        totalBenefit = totalCost-totalPrice;
     }
     @Override
     public boolean addAll(Collection<? extends StockableProduct> c) {
+        return addAll(size(), c);
+    } // vv FALL THROUGH vv
+    @Override
+    public boolean addAll(int index, Collection<? extends StockableProduct> c) {
         boolean out = false;
+        int i = index;
         for (StockableProduct product : c) {
-            out = add(product) || out;
+            add(i++, product);
+            out = true;
         }
+        return out;
+    }
+    @Override
+    public boolean remove(Object o) {
+        try {
+            remove(indexOf(o));
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+    } // vv FALL THROUGH vv
+    @Override
+    public StockableProduct remove(int index) {
+        StockableProduct out = list.remove(index);
+        getters.remove(Integer.toString(index));
+        setters.remove(Integer.toString(index));
+        totalCost -= out.getTotalCost();
+        totalPrice -= out.getTotalPrice();
+        totalBenefit = totalCost-totalPrice;
         return out;
     }
     @Override
@@ -160,6 +166,14 @@ public class ProductList extends WarehouseElement implements Collection<Stockabl
             out = remove(product) || out;
         }
         return out;
+    }
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
     }
     @Override
     public boolean retainAll(Collection<?> c) {
@@ -176,8 +190,41 @@ public class ProductList extends WarehouseElement implements Collection<Stockabl
         list.clear();
         totalCost = totalPrice = totalBenefit = 0;
     }
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+    @Override
+    public Iterator<StockableProduct> iterator() {
+        return list.iterator();
+    }
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+    @Override
+    public ListIterator<StockableProduct> listIterator() {
+        return list.listIterator();
+    }
+    @Override
+    public ListIterator<StockableProduct> listIterator(int index) {
+        return list.listIterator(index);
+    }
+    @Override
+    public List<StockableProduct> subList(int fromIndex, int toIndex) {
+        return list.subList(fromIndex, toIndex);
+    }
 
 
+    // IO
     public static ProductList readFromString(String string) {
         return new ProductList(string);
     }
@@ -187,5 +234,11 @@ public class ProductList extends WarehouseElement implements Collection<Stockabl
     }
     public static ProductList readFromFile(String filepath) {
         return new ProductList(stringsFromFile(filepath));
+    }
+
+    @Override
+    public void writeToFile(String filepath) {
+        String string = String.join("\n", list.stream().map((StockableProduct product) -> product.toString()).toArray(String[]::new));
+        stringToFile(filepath, string);
     }
 }
