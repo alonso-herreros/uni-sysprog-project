23169fe00b62fcd9039b46d21c61ddfbea35914d
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-05-30 14:48:53 +0200

    Remove unused imports in ProductListTest

--- src/test/java/store/ProductListTest.java
+++ src/test/java/store/ProductListTest.java
@@ -1,16 +1,9 @@
 package store;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 
