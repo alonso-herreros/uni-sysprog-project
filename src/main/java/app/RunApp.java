0e1d948aa13a9f9091da8cc03022f2d398d48d9c
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-08 00:50:58 +0200

    Allow store creation

--- src/main/java/app/RunApp.java
+++ src/main/java/app/RunApp.java
@@ -8,26 +8,31 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
 @RestController
 public class RunApp {
 
-    static StoreManager storeManager = new StoreManager("unnamed", "");
+    static final String DATA_DIR = "data";
+
+    static StoreManager storeManager = new StoreManager("unnamed", DATA_DIR);
 
     public static void main(String[] args) throws SAXException, IOException {
       SpringApplication.run(RunApp.class, args);
     }
 
     @PostMapping("/create-store")
-    public static String createStore(@RequestParam(value = "name", defaultValue = "Store") String name) {
-        storeManager = new StoreManager(name, "");
-        return "Store created";
+    public static ModelAndView createStore(ModelMap model, @RequestParam(value = "storeName", defaultValue = "Store") String name) {
+        storeManager = new StoreManager(name, DATA_DIR);
+        model.addAttribute("from", "create-store");
+        return new ModelAndView("redirect:/", model);
     }
 
     @GetMapping("/store/getvar")
