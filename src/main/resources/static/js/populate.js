c5920697fb126b7070e1d213095ffe6eab6d0c41
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-08 13:51:06 +0200

    Use " instead of '

--- src/main/resources/static/js/populate.js
+++ src/main/resources/static/js/populate.js
@@ -2,38 +2,38 @@ window.onload = populateTable;
 
 
 function populateTable() {
-  // Make an AJAX request to retrieve the data from your server
+  // Make AJAX requests to retrieve the data from server
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'list/listObject', true);
+  xhr.open("GET", "list/listObject", true);
   xhr.onload = function () {
     if (xhr.status === 200) {
       var data = JSON.parse(xhr.responseText);
       
       // Clear the existing table rows
-      var tableBody = document.getElementById('element-table-body');
-      tableBody.innerHTML = '';
+      var tableBody = document.getElementById("element-table-body");
+      tableBody.innerHTML = "";
 
       // Loop through the data and create table rows dynamically
       data.list.forEach(function (item) {
-        var row = document.createElement('tr');
+        var row = document.createElement("tr");
 
         // Create table cells for each data field
-        var idCell = document.createElement('td');
+        var idCell = document.createElement("td");
         idCell.textContent = item.productID;
         row.appendChild(idCell);
 
-        var nameCell = document.createElement('td');
+        var nameCell = document.createElement("td");
         nameCell.textContent = item.name;
         row.appendChild(nameCell);
 
-        var brandCell = document.createElement('td');
+        var brandCell = document.createElement("td");
         brandCell.textContent = item.brand;
         row.appendChild(brandCell);
 
-        var buttonCell = document.createElement('td');
-        var button = document.createElement('button');
-        button.setAttribute('type', 'button');
-        button.setAttribute('class', 'open-button');
+        var buttonCell = document.createElement("td");
+        var button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "open-button");
         button.innerHTML = '<i class="fas fa-xl fa-circle-info"></i>';
         buttonCell.appendChild(button);
         row.appendChild(buttonCell);
