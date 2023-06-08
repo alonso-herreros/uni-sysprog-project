8945ce3b7663bc2d879e55aa302afec543140b3a
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-08 19:23:34 +0200

    Make more Order setters public

--- src/main/java/store/Order.java
+++ src/main/java/store/Order.java
@@ -82,16 +82,16 @@ public class Order extends ProductList {
     }
 
     public int getOrderID() { return orderID; }
-    private void setOrderID(int orderID) {
+    public void setOrderID(int orderID) {
         this.orderID = orderID;
         maxOrderID = Math.max(maxOrderID, orderID);
     }
 
     public Person getClient() { return client; }
-    private void setClient(Person client) { this.client = client; }
+    public void setClient(Person client) { this.client = client; }
 
     public Person getEmployee() { return employee; }
-    private void setEmployee(Person employee) { this.employee = employee; }
+    public void setEmployee(Person employee) { this.employee = employee; }
     
     public String getDir() { return dir; }
     public void setDir(String dir) {
