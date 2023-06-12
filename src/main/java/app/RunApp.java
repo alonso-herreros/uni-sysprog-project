916a0032e7d7573cd1b9c6155719487a1e937454
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-12 19:46:48 +0200

    Add store creation, loading and overwriting

--- src/main/java/app/RunApp.java
+++ src/main/java/app/RunApp.java
@@ -4,17 +4,20 @@ import org.xml.sax.SAXException;
 
 import store.StoreManager;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.ModelMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
@@ -31,10 +34,63 @@ public class RunApp {
     }
 
     @PostMapping("/create")
-    public static ModelAndView createStore(ModelMap model, @RequestParam(value = "storeName", defaultValue = "Store") String name) {
-        storeManager = new StoreManager(name, DATA_DIR);
-        model.addAttribute("feedback", "newStore");
-        return new ModelAndView("redirect:home", model);
+    public static ResponseEntity<?> createStore(
+            @RequestParam(value = "storeName", defaultValue = "Store") String storeName,
+            @RequestParam(value = "force", defaultValue = "false") boolean force) {
+
+        Map<String, Object> responseBody = new HashMap<String, Object>();
+        responseBody.put("storeName", storeName);
+
+        if (storeName.equals("")) {
+            responseBody.put("message", "Store name cannot be empty.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        }
+
+        File storeDir = new File(DATA_DIR + File.separator + storeName);
+        if (storeDir.exists()) {
+            if (!force) {
+                responseBody.put("message", "Store already exists.");
+                responseBody.put("errCode", "STORE_EXISTS");
+                return ResponseEntity.status(HttpStatus.MULTIPLE_CHOICES).body(responseBody);
+            }
+            if (!deleteFile(storeDir)) {
+                responseBody.put("message", "Store could not be deleted.");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+            }
+            else if (storeDir.exists()) {
+                responseBody.put("message", "Store was deleted, but it was not.");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+            }
+            return createStore(storeName, false);
+        }
+
+        storeManager = new StoreManager(storeName, DATA_DIR);
+        responseBody.put("message", "Store created.");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+    }
+
+    @PostMapping("/load")
+    public static ResponseEntity<?> loadStore(
+            @RequestParam(value = "storeName", defaultValue = "Store") String storeName) {
+
+        Map<String, Object> responseBody = new HashMap<String, Object>();
+        responseBody.put("storeName", storeName);
+
+        if (storeName.equals("")) {
+            responseBody.put("message", "Store name cannot be empty.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        }
+
+        File storeDir = new File(DATA_DIR + File.separator + storeName);
+        if (!storeDir.exists()) {
+            responseBody.put("message", "Store does not exist.");
+            responseBody.put("errCode", "STORE_NOT_FOUND");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+        }
+
+        storeManager = new StoreManager(storeName, DATA_DIR);
+        responseBody.put("message", "Store loaded.");
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
     @GetMapping("/getvar")
@@ -77,5 +133,14 @@ public class RunApp {
     }
     // #endregion
 
+    public static boolean deleteFile(File dir) {
+        System.out.println(dir);
+        if (dir.isDirectory()) {
+            for (File child : dir.listFiles()) {
+                deleteFile(child);
+            }
+        }
+        return dir.delete();
+    }
 
 }
---
+++ src/main/resources/static/js/create.js
@@ -0,0 +1,156 @@
+
+window.onload = () => {
+  // Can't simply use on("submit", function) because it hides the submitter
+  // Either of these work.
+  //$("#create-form").on("submit", (e) => sendCreateRequest(e.originalEvent))
+  $("#create-form")[0].addEventListener("submit", submitCreateForm)
+  $("#create-form")[0].addEventListener("reset", cancel)
+
+  $("#create-form button[name='cancel']").on("click", cancel)
+}
+
+function submitCreateForm(event) {
+  event.preventDefault()
+
+  const form = event.target
+  disableForm(form)
+
+  sendCreateForm(form, event.submitter.value || "create").then((response) => {
+    if (response)  enableForm(form)
+  })
+}
+
+function sendCreateForm(form, action) {
+  var force
+  switch(action) {
+    case "create-f":
+      force = true
+    case "create":
+      return sendCreateRequest(form, force)
+    case "load":
+      return sendLoadRequest(form)
+    default:
+      return "Unknown form action: " + action
+  }
+}
+
+
+async function sendCreateRequest(form, force=false) {
+  const formData = new FormData(form)
+  formData.append("force", force)
+  
+  return await fetch('/store/create', {
+    method: 'POST',
+    body: formData
+  }).then( (response) =>
+    processCreateResponse(form, response)
+  )
+}
+
+async function sendLoadRequest(form) {
+  return await fetch('/store/load', {
+    method: 'POST',
+    body: new FormData(form)
+  }).then( (response) =>
+    processLoadResponse(form, response)
+  )
+}
+
+
+/**
+ * 
+ * @param {*} form The form that contains the sent data
+ * @param {Response} response The response from the server
+ * @returns Whether the request is resolved and the form should be enabled
+ */
+async function processCreateResponse(form, response) {
+  const [status, resp] = await preprocessResponse(response)
+
+  switch (status) {
+    case 201: // Created
+      showVarToast("Store '" + resp.storeName + "' created.")
+      return true
+    case 300: // Store already exists (probably)
+      if (resp.errCode == "STORE_EXISTS") {
+        showLoadPopup(form, resp.storeName)
+        return false
+      }
+      // Fall through. Don't know how you could get here.
+    default: // Includes 500: Internal server error and false from bad JSON
+      showErrorToast(resp.message)
+  }
+  return true
+}
+
+/**
+ * 
+ * @param {*} form The form that contains the sent data
+ * @param {Response} response The response from the server
+ * @returns Whether the request is resolved and the form should be enabled
+ */
+async function processLoadResponse(form, response) {
+  const [status, resp] = await preprocessResponse(response)
+
+  switch (status) {
+    case 200: // Loaded
+      showVarToast("Store '" + resp.storeName + "' loaded.")
+      return true
+    case 404: // Store not found
+      if (resp.errCode == "STORE_NOT_FOUND") {
+        showVarToast("Store not found. Try creating it first.")
+        return true
+      }
+      // Fall through. Don't know how you could get here.
+    default: // Includes 500: Internal server error and false from bad JSON
+      showErrorToast(resp.message)
+  }
+  return true
+}
+
+
+async function preprocessResponse(response, log=false) {
+  const status = response.status
+  const respRaw = await response.text()
+  if (log)  console.log("Response: " + status + " " + respRaw)
+
+  try {
+    return [status, JSON.parse(respRaw)]
+  }
+  catch (e) {
+    return [false, {message: "Server sent invalid JSON: " + respRaw}]
+  }
+}
+
+
+function cancel(event) {
+  event.preventDefault()
+  const form = event.target
+  hideLoadPopup(form)
+  enableForm(form)
+}
+
+
+function disableForm(form) {
+  form.classList.add('processing')
+  form.disabled = true
+  $("#submit-buttons button", form).prop("disabled", true)
+}
+function enableForm(form) {
+  hideLoadPopup(form)
+  form.classList.remove('processing')
+  form.disabled = false
+  $("#submit-buttons button", form).prop("disabled", false)
+}
+
+function showLoadPopup(form, storeName) {
+  $("#popup #var-storeName")[0].innerText = storeName
+  $("#popup", form)[0].classList.add("show")
+}
+function hideLoadPopup(form) {
+  $("#popup", form)[0].classList.remove("show")
+}
+
+function showErrorToast(serverMessage) {
+  showVarToast("Server said: " + serverMessage)
+  return true
+}
