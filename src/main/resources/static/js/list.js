fa57e6fe13e1d0cf6ee9f620560f9c377484b3e2
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-20 15:51:37 +0200

    Store list in global variable

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -5,30 +5,31 @@ import {
 
 const STORE_PATH = "/store"
 const LIST_PATHEXT = "/list-object"
-const DETAILS_PATH = "/details"
-const COLUMNS_PATHEXT = "/columns"
+
+var list
 
 
 $(document).ready(async ()=> {
-  populateList()
+  await populateList(list)
 })
 
 
+async function fetchListObject(context) {
+  const listPath = `${STORE_PATH}/${context}${LIST_PATHEXT}`
+  list = await fetch(listPath).then( (response) =>
+    response.json()
+  )
+  return list
+}
+
+
 async function populateList() {
-  var list = await fetchListObject(VARIANT.name)
+  if (!list)  await fetchListObject(VARIANT.name)
   if (!list.length)  list = Object.values(list) // Convert object to array
   populateTable(
     $(".context-table")[0],
     TABLE_COLS,
     list
-  )
-}
-
-
-async function fetchListObject(context) {
-  const listPath = `${STORE_PATH}/${context}${LIST_PATHEXT}`
-  return fetch(listPath).then( (response) =>
-    response.json()
   )
 }
 
