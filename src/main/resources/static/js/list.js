a52833765f8353e3cffc5a7b3f070c74d0bd775d
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-21 00:38:16 +0200

    Add basic side menu field population

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,5 +1,6 @@
 import {
-  populateTable
+  populateTable,
+  newElement
 } from "./populate-table.js"
 
 
@@ -7,6 +8,7 @@ const STORE_PATH = "/store"
 const LIST_PATHEXT = "/list-object"
 
 var list
+var selectedElementID
 
 
 $(document).ready(async ()=> {
@@ -42,13 +44,19 @@ function openDetailsModal(event) {
   const button = event.target
   const selectedRow = button.closest("tr");
   const sideMenu = $("#side-menu")
+  const elementId = selectedRow.dataset["elementID"]
+  if (elementId == selectedElementID) {
+    toggleSideMenu(sideMenu, false)
+    selectedElementID = null
+    return
+  }
 
-  toggleSideMenu(sideMenu)
+  toggleSideMenu(sideMenu, true)
 
-  const elementId = 2 //button.getAttribute("data-element-id")
-  var element = list.filter((element) => (element.productID == elementId))[0]
+  selectedElementID = elementId
+  const element = list.filter((element) => (element.productID == elementId))[0]
   console.log(element)
-  //populateSideMenu(sideMenu, element)
+  populateSideMenu($(".content", sideMenu), element)
 }
 
 function toggleSideMenu(sideMenu, show) {
@@ -64,4 +72,27 @@ function toggleSideMenu(sideMenu, show) {
 function toggleMenuContent(menuContent, show) {
   menuContent.toggleClass("hide", !show)
   setTimeout(() => menuContent.toggleClass("show", show), 1)
+}
+
+function populateSideMenu(sideMenuContent, element) {
+  const detailsForm = $(".details-form", sideMenuContent)[0]
+  detailsForm.innerHTML = ""
+  for (const [fieldName, fieldValue] of Object.entries(element)) {
+    detailsForm.appendChild(buildSideMenuField(fieldName, fieldValue))
+  }
+}
+
+function buildSideMenuField(fieldName, fieldValue) {
+  const label = newElement("label", fieldName)
+  label.setAttribute("for", fieldName)
+  const input = newElement("input")
+  input.setAttribute("type", "text")
+  input.setAttribute("name", fieldName)
+  input.setAttribute("value", fieldValue)
+  input.setAttribute("readonly", true)
+
+  const fieldContainer = newElement("div", null, "field")
+  fieldContainer.appendChild(label)
+  fieldContainer.appendChild(input)
+  return fieldContainer
 }
