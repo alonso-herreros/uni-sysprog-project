5143d68c2778d75ad37235826c13877bc5b29570
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-06-21 16:47:41 +0200

    Update parseJsonResponse return object

--- src/main/resources/static/js/comms.js
+++ src/main/resources/static/js/comms.js
@@ -7,9 +7,12 @@ export {
  * Interprets a server response, expected to be JSON.
  * 
  * @param {Response} response Server response
- * @returns [status, response] where status is the HTTP status code
+ * @returns {[number, Object]} [status, response] where status is the HTTP status code
  * and response is the JSON response. If the response is not valid JSON,
- * status is false and response is {message: "Server sent invalid JSON: " + respRaw}
+ * status is false and response is the object:
+ * ```
+ * {status: status, message: `Server sent invalid JSON: ${respRaw}`}
+ * ```
  */
 async function parseJsonResponse(response) {
   const status = response.status
@@ -19,7 +22,7 @@ async function parseJsonResponse(response) {
     return [status, JSON.parse(respRaw)]
   }
   catch (e) {
-    return [false, {message: "Server sent invalid JSON: " + respRaw}]
+    return [false, {status: status, message: `Server sent invalid JSON: ${respRaw}`}]
   }
 }
 
